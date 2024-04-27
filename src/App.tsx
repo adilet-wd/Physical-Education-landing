d19d620d273f2140ceaf25fded530b2311d44d8e
Adilet <usenkanov.adilet.kanatbekovich@gmail.com>
2024-04-27 20:54:18 +0600

    Инициализация vite react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react';
+import './styles/App.scss';
+import './styles/breakpoints.scss';
+import './styles/clear.scss';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
