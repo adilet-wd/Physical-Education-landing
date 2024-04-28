7c6f0f068d5f933bd0654d853da6006374e03b89
Adilet <usenkanov.adilet.kanatbekovich@gmail.com>
2024-04-28 13:36:06 +0600

    Скрол к началу страницы при навигации

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './styles/App.scss';
 import './styles/breakpoints.scss';
 import './styles/clear.scss';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home/Home';
 import Basketball from './pages/Clubs/Basketball/Basketball';
 import Volleyball from './pages/Clubs/Volleyball/Volleyball';
 import Football from './pages/Clubs/Football/Football';
 import Chess from './pages/Clubs/Chess/Chess';
+import { useEffect } from 'react';
+
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return (
     <Routes>
       <Route path="/" element={<Layout/>}>
